e616d0f0ceaacf9c3953c82a45866401f429d040
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2019-01-11 00:01:36 +0700

    add insert and select query

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sqflite/database/dbhelper.dart';
+import 'package:flutter_sqflite/model/employee.dart';
+import 'package:flutter_sqflite/employeelist.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter SQFlite',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Flutter SQFlite'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -43,67 +27,97 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Employee employee = new Employee("", "", "", "");
+
+  String firstname;
+  String lastname;
+  String emailId;
+  String mobileno;
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+  final formKey = new GlobalKey<FormState>();
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
+      key: scaffoldKey,
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+          title: new Text('Tambah Pegawai'),
+          actions: <Widget>[
+            new IconButton(
+              icon: const Icon(Icons.view_list),
+              tooltip: 'Next choice',
+              onPressed: () {
+                navigateToEmployeeList();
+              },
+            ),
+          ]
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: new Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: new Form(
+          key: formKey,
+          child: new Column(
+            children: [
+              new TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: new InputDecoration(labelText: 'Nama Awal'),
+                validator: (val) =>
+                val.length == 0 ?"Masukan Nama Awak" : null,
+                onSaved: (val) => this.firstname = val,
+              ),
+              new TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: new InputDecoration(labelText: 'Nama Akhir'),
+                validator: (val) =>
+                val.length ==0 ? 'Masukan Nama Akhir' : null,
+                onSaved: (val) => this.lastname = val,
+              ),
+              new TextFormField(
+                keyboardType: TextInputType.phone,
+                decoration: new InputDecoration(labelText: 'No Telephone'),
+                validator: (val) =>
+                val.length ==0 ? 'Masukan No Telephone' : null,
+                onSaved: (val) => this.mobileno = val,
+              ),
+              new TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: new InputDecoration(labelText: 'Alamat Email'),
+                validator: (val) =>
+                val.length ==0 ? 'Masukan Alamat Email' : null,
+                onSaved: (val) => this.emailId = val,
+              ),
+              new Container(margin: const EdgeInsets.only(top: 10.0),child: new RaisedButton(onPressed: _submit,
+                child: new Text('Simpan'),),)
+
+            ],
+          ),
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+  void _submit() {
+    if (this.formKey.currentState.validate()) {
+      formKey.currentState.save();
+    }else{
+      return null;
+    }
+    var employee = Employee(firstname,lastname,mobileno,emailId);
+    var dbHelper = DBHelper();
+    dbHelper.saveEmployee(employee);
+    _showSnackBar("Data Berhasil Tersimpan");
+  }
+
+  void _showSnackBar(String text) {
+    scaffoldKey.currentState
+        .showSnackBar(new SnackBar(content: new Text(text)));
+  }
+
+  void navigateToEmployeeList(){
+    Navigator.push(
+      context,
+      new MaterialPageRoute(builder: (context) => new MyEmployeeList()),
     );
   }
 }
